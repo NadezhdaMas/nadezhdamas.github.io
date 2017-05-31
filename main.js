3c46d607e4d24927c1ecc008528d7e237707d1b5
NadezhdaMas <NadVMas@gmail.com>
2017-05-31 12:03:47 +0300

    les1homework

---
+++ main.js
@@ -0,0 +1,17 @@
+var x = document.getElementById("number1")
+var y = document.getElementById("number2")
+document.write(text="</br>");
+console.log(x,y)
+console.log(document.getElementById("1"));
+document.getElementById("submit").addEventListener("click", function() {
+  document.write(+x.value + +y.value);
+})
+
+var x = document.getElementById("number1")
+var y = document.getElementById("number2")
+document.write(text="</br>");
+console.log(x,y)
+console.log(document.getElementById("1"));
+document.getElementById("submit").addEventListener("click", function() {
+  document.write(+x.value + +y.value);
+})
